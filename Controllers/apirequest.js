c1bd0b8b3ad3da696169277cfa3deeb3c14a4d50
LongBeachHXC <fweinberg@live.com>
2018-03-02 20:43:22 -0800

    Bot Testing

--- Controllers/apirequest.js
+++ Controllers/apirequest.js
@@ -1,15 +1,15 @@
 const RequestClient = require("reqclient").RequestClient;
 
-const retrieveMessageData = new RequestClient({
+let retrieveMessageData = new RequestClient({
     "baseUrl": "https://api.ciscospark.com/v1/messages/",
     "headers": {"Authorization" : "Bearer NThjMDczMDYtNjE5Yi00MTA4LWFlMjQtZTI0Yzc4NzQ3MDFmMWJlZDBkMzMtYzUy"}
 })
 
-const retrieveCurrencyPrice  = new RequestClient({
+let retrieveCurrencyPrice  = new RequestClient({
     "baseUrl": "https://api.binance.com/api/v3/ticker/"
 })
 
-const sendMessage = new RequestClient({
+let sendMessage = new RequestClient({
     "baseUrl": "https://api.ciscospark.com/v1/",
     "headers": {"Authorization" : "Bearer NThjMDczMDYtNjE5Yi00MTA4LWFlMjQtZTI0Yzc4NzQ3MDFmMWJlZDBkMzMtYzUy"}
 })
@@ -27,8 +27,7 @@ const DirectRoomResponse = (trigger) => {
                 "toPersonId": trigger.data.personId,
                 "text": "The current price for " + currencySymbol.text + " is " + currencyPrice.price
             })
-            .then(resp => {
-                console.log(resp);
+            .then(console.log);
             })
             .catch(console.log);
         })
@@ -40,28 +39,52 @@ const DirectRoomResponse = (trigger) => {
 const GroupRoomResponse = (trigger) => {
     let currencyPair = "";
     retrieveMessageData.get(trigger.data.id)
-    .then(currencySymbol =>  {
-        currencyPair = currencySymbol.text.split(" ")[1]
-        retrieveCurrencyPrice.get({"uri":"price",
+    .then(currencySymbol => {
+        currencyPair = currencySymbol.text.split(" ")[1];
+        retrieveCurrencyPrice.get({
+            "uri": "price",
             "query": {
                 "symbol": currencyPair
             }
+        })
         .then(currencyPrice => {
-            sendMessage.post("messages", {
-                "roomId": currencySymbol.roomId,
-                "markdown": "Hello <@personEmail:" + currencySymbol.personEmail + ">, the curret price for currencty pair " + currencyPair + " is " + currencyPrice.price
-            })
-            .then(resp => {
-                console.log(resp)
-            })
+            sendMessage.post(
+                "messages", {
+                    "roomId": currencySymbol.roomId
+                    "markdown": "The current price for " + currencyPair " is " + currencyPrice.price
+                })
             .catch(console.log)
         })
         .catch(console.log)
     })
     .catch(console.log)
-})
-.catch(console.log)
 }
+
+
+
+//     retrieveMessageData.get(trigger.data.id)
+//     .then(currencySymbol =>  {
+//         currencyPair = currencySymbol.text.split(" ")[1]
+//         retrieveCurrencyPrice.get({"uri":"price",
+//             "query": {
+//                 "symbol": currencyPair
+//             }
+//         .then(currencyPrice => {
+//             sendMessage.post("messages", {
+//                 "roomId": currencySymbol.roomId,
+//                 "markdown": "Hello <@personEmail:" + currencySymbol.personEmail + ">, the curret price for currencty pair " + currencyPair + " is " + currencyPrice.price
+//             })
+//             .then(resp => {
+//                 console.log(resp)
+//             })
+//             .catch(console.log)
+//         })
+//         .catch(console.log)
+//     })
+//     .catch(console.log)
+// })
+// .catch(console.log)
+// }
 
 
     // retrieveMessageData.get(trigger.data.id)
